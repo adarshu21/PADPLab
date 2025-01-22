5e6eaa60f94558371196dbbe46478005bfbd6381
Shreyas-Shankar155 <shreyas.shankar2018@gmail.com>
2025-01-22 19:56:51 +0530

    Adding comments to example code

--- PartB/CUDA_Codes/count_1.cu
+++ PartB/CUDA_Codes/count_1.cu
@@ -1,29 +1,26 @@
+/*
+Counts number of 1s in an array
+*/
 #include <stdio.h>
 #include <stdlib.h>
 
-
-#define SIZE 1000
-/*
-__global__ void helloKernel() {
-    printf("Hello, CUDA!\n");
-}
-*/
+#define SIZE 100000
 
 __global__ void count1s(int *arr, int *res)
 	{
-	__shared__ int x;
-	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	__shared__ int val; //creates shared memory for all threads in thread block
+	int tid = threadIdx.x + blockIdx.x * blockDim.x; //find index according to array
 	
 	if(threadIdx.x==0)
-		x = 0;
+		val = 0; //per block, assign one thread to value 0
 	__syncthreads();
 	
-	if(tid<SIZE && arr[tid]==1)
-		atomicAdd(&x,1);
+	if(tid<SIZE && arr[tid]==1) //per block, keep sum of number of 1s
+		atomicAdd(&val,1); 
 	__syncthreads();
 	
-	if (threadIdx.x == 0) {
-        	atomicAdd(res, x);
+	if (threadIdx.x == 0) { //master thread per block updates result variable
+        	atomicAdd(res, val);
     		}
 	}
 
@@ -36,20 +33,19 @@ int main() {
 	printf("Number of 1s from CPU: %d\n",myRes);	
   	myRes=0;  
   	
-    int *res;
-    int *arr;
-    
-    cudaMalloc(&arr, sizeof(A));
-    
+    int *res; //stores result
+    int *arr; //stores array A
+        
     cudaMalloc(&res, sizeof(int));
     cudaMemset(res, 0, sizeof(int));
     
+    cudaMalloc(&arr, sizeof(A));
     cudaMemcpy(arr, A, sizeof(A), cudaMemcpyHostToDevice);
     
     int blocks = (int)(SIZE/64)+1;
     count1s<<<blocks,64>>> (arr, res);
     
-    cudaDeviceSynchronize();
+//    cudaDeviceSynchronize();
     
     cudaMemcpy(&myRes, res, sizeof(int), cudaMemcpyDeviceToHost);
     
